fde9cccf9f179cc9bb7a62e822510dfd63365b91
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 12:08:34 -0500

    Page 45: Replacing the Conditional Logic on Price Code with Polymorphism (added NewReleasePrice.charge and ChildrensPrice.charge and changed Movie.charge to simply delegate to these)

--- chapter_1.rb
+++ chapter_1.rb
@@ -20,17 +20,7 @@ class Movie
 	end
 	
 	def charge(days_rented)
-		result = 0
-		case price_code
-		when Movie::REGULAR
-			return @price.charge(days_rented)
-		when Movie::NEW_RELEASE
-			result += days_rented * 3
-		when Movie::CHILDRENS
-			result += 1.5
-			result += (days_rented - 3) * 1.5 if days_rented > 3
-		end
-		result
+		@price.charge(days_rented)
 	end
 	
 	def frequent_renter_points(days_rented)
@@ -47,9 +37,17 @@ class RegularPrice
 end
 
 class NewReleasePrice
+	def charge(days_rented)
+		days_rented * 3
+	end
 end
 
 class ChildrensPrice
+	def charge(days_rented)
+		result = 1.5
+		result += (days_rented - 3) * 1.5 if days_rented > 3
+		result
+	end
 end
 
 class Rental
