e060be0b82cba3876ffc2a79ca2a46ef39278c58
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 11:20:35 -0500

    Page 19: Decomposing and Redistributing the Statement Method (moving frequent_renter_points calculation from Customer.statement to Rental.frequent_renter_points)

--- chapter_1.rb
+++ chapter_1.rb
@@ -32,6 +32,10 @@ class Rental
 		end
 		result
 	end
+	
+	def frequent_renter_points
+		(movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+	end
 end
 
 
@@ -51,13 +55,8 @@ class Customer
 		total_amount, frequent_renter_points = 0, 0
 		result = "Rental Record for #{@name}\n"
 		@rentals.each do |element|
-			
-			# add frequent renter points
-			frequent_renter_points += 1
-			# add bonus for a two day new release rental
-			if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-				frequent_renter_points += 1
-			end
+			frequent_renter_points += element.frequent_renter_points
+
 			# show figures for this rental
 			result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
 			total_amount += element.charge
