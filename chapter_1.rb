ed81f33c408d5506370850eadb010945c249e4a7
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 11:27:25 -0500

    Page 27: Decomposing and Redistributing the Statement Method (removed frequent_renter_points temp counter from Customer.statement to Customer.total_frequent_renter_points)

--- chapter_1.rb
+++ chapter_1.rb
@@ -55,14 +55,12 @@ class Customer
 		frequent_renter_points = 0
 		result = "Rental Record for #{@name}\n"
 		@rentals.each do |element|
-			frequent_renter_points += element.frequent_renter_points
-
 			# show figures for this rental
 			result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
 		end
 		#add footer lines
 		result += "Amount owed is #{total_charge}\n"
-		result += "You earned #{frequent_renter_points} frequent renter points"
+		result += "You earned #{total_frequent_renter_points} frequent renter points"
 		result
 	end
 	
@@ -70,6 +68,10 @@ class Customer
 	
 	def total_charge
 		@rentals.inject(0) {|sum, element| sum + element.charge}
+	end
+	
+	def total_frequent_renter_points
+		@rentals.inject(0) {|sum, element| sum + element.frequent_renter_points}
 	end
 end
 
