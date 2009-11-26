cc400b0c99d8b0249ac0ba3867437daa5a59e709
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 11:25:35 -0500

    Page 25: Decomposing and Redistributing the Statement Method (shortened Customer.total_charge method using Collection Closure Method and inject)

--- chapter_1.rb
+++ chapter_1.rb
@@ -69,11 +69,7 @@ class Customer
 	private
 	
 	def total_charge
-		result = 0
-		@rentals.each do |element|
-			result += element.charge
-		end
-		result
+		@rentals.inject(0) {|sum, element| sum + element.charge}
 	end
 end
 
