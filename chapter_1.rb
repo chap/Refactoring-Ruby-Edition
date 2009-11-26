d63cbdb6f9ecc038abb22c63060f5b5a86fec7cc
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 10:59:20 -0500

    Page 9: Decomposing and Redistributing the Statement Method

--- chapter_1.rb
+++ chapter_1.rb
@@ -36,19 +36,7 @@ class Customer
 		total_amount, frequent_renter_points = 0, 0
 		result = "Rental Record for #{@name}\n"
 		@rentals.each do |element|
-			this_amount = 0
-			
-			# determine amounts for each line
-			case element.movie.price_code
-			when Movie::REGULAR
-				this_amount += 2
-				this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-			when Movie::NEW_RELEASE
-				this_amount += element.days_rented * 3
-			when Movie::CHILDRENS
-				this_amount += 1.5
-				this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-			end
+			this_amount = amount_for(element)
 			
 			# add frequent renter points
 			frequent_renter_points += 1
@@ -64,6 +52,21 @@ class Customer
 		result += "Amount owed is #{total_amount}\n"
 		result += "You earned #{frequent_renter_points} frequent renter points"
 		result
+	end
+	
+	def amount_for(element)
+		this_amount = 0
+		case element.movie.price_code
+		when Movie::REGULAR
+			this_amount += 2
+			this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+		when Movie::NEW_RELEASE
+			this_amount += element.days_rented * 3
+		when Movie::CHILDRENS
+			this_amount += 1.5
+			this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+		end
+		this_amount
 	end
 end
 
