80cd752e59d143fa7a69952aebb35228df31bded
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 11:05:22 -0500

    Page 13: Decomposing and Redistributing the Statement Method (moving the Customer.amount_for(rental) method to Rental.charge)

--- chapter_1.rb
+++ chapter_1.rb
@@ -17,6 +17,21 @@ class Rental
 	def initialize(movie, days_rented)
 		@movie, @days_rented = movie, days_rented
 	end
+	
+	def charge
+		result = 0
+		case movie.price_code
+		when Movie::REGULAR
+			result += 2
+			result += (days_rented - 2) * 1.5 if days_rented > 2
+		when Movie::NEW_RELEASE
+			result += days_rented * 3
+		when Movie::CHILDRENS
+			result += 1.5
+			result += (days_rented - 3) * 1.5 if days_rented > 3
+		end
+		result
+	end
 end
 
 
@@ -55,18 +70,7 @@ class Customer
 	end
 	
 	def amount_for(rental)
-		result = 0
-		case rental.movie.price_code
-		when Movie::REGULAR
-			result += 2
-			result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-		when Movie::NEW_RELEASE
-			result += rental.days_rented * 3
-		when Movie::CHILDRENS
-			result += 1.5
-			result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-		end
-		result
+		rental.charge
 	end
 end
 
