c569984158e20b32f6e721dc85f0e5fde574c452
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 12:19:21 -0500

    Page 47: Replacing the Conditional Logic on Price Code with Polymorphism (moving frequent_renter_points to Price classes, added module for DefaultPrice and included in RegularPrice and ChildrensPrice)

--- chapter_1.rb
+++ chapter_1.rb
@@ -24,11 +24,19 @@ class Movie
 	end
 	
 	def frequent_renter_points(days_rented)
-		(price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+		@price.frequent_renter_points(days_rented)
+	end
+end
+
+module DefaultPrice
+	def frequent_renter_points(days_rented)
+		1
 	end
 end
 
 class RegularPrice
+	include DefaultPrice
+	
 	def charge(days_rented)
 		result = 2
 		result += (days_rented - 2) * 1.5 if days_rented > 2
@@ -40,9 +48,15 @@ class NewReleasePrice
 	def charge(days_rented)
 		days_rented * 3
 	end
+	
+	def frequent_renter_points(days_rented)
+		days_rented > 1 ? 2 : 1
+	end
 end
 
 class ChildrensPrice
+	include DefaultPrice
+	
 	def charge(days_rented)
 		result = 1.5
 		result += (days_rented - 3) * 1.5 if days_rented > 3
