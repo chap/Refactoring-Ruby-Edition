8a521db0c6bb37c865259921dd856602cbc79cd3
Chap Ambrose <chapambrose@gmail.com>
2009-11-26 11:23:39 -0500

    Page 23: Decomposing and Redistributing the Statement Method (removing total_amount temp counter from Customer.statement method to Customer.total_charge)

--- chapter_1.rb
+++ chapter_1.rb
@@ -52,18 +52,27 @@ class Customer
 	end
 	
 	def statement
-		total_amount, frequent_renter_points = 0, 0
+		frequent_renter_points = 0
 		result = "Rental Record for #{@name}\n"
 		@rentals.each do |element|
 			frequent_renter_points += element.frequent_renter_points
 
 			# show figures for this rental
 			result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-			total_amount += element.charge
 		end
 		#add footer lines
-		result += "Amount owed is #{total_amount}\n"
+		result += "Amount owed is #{total_charge}\n"
 		result += "You earned #{frequent_renter_points} frequent renter points"
+		result
+	end
+	
+	private
+	
+	def total_charge
+		result = 0
+		@rentals.each do |element|
+			result += element.charge
+		end
 		result
 	end
 end
